bea27a4f74752d311187e1d9eceb611494023c4b
Kristy Hamlin <khaml004@fiu.edu>
2023-05-05 13:50:40 -0400

    update test

--- src/JobAdReader.java
+++ src/JobAdReader.java
@@ -1,4 +1,24 @@
+/*
+ * TechTogether Miami 2023 Hackathon
+ * 5/5/2023
+ * Ishel Zain, Thi Pham, Kristy Hamlin
+ * 
+ * This Class will read a text file representing
+ * a job ad to be compared to job postings to 
+ * calculate a match percentage and hiring probability. 
+ *  
+ */
+import java.io.File;
+import java.util.Scanner;
+
 
 public class JobAdReader {
+	String path;
+	int desiredYears;
+	File file = new File(path);
+	Scanner scnr = new Scanner(file);
+	
+	//This is a new comment
+	
 
 }
